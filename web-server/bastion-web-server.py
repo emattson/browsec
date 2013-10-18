67c01c6c13e343fe71f63795438daf151fa9ddd3
Eli Mattson <emattson@gwu.edu>
2013-10-18 19:31:13 -0400

    add the bastion-web-server

---
+++ web-server/bastion-web-server.py
@@ -0,0 +1,20 @@
+#Simple web server
+import SimpleHTTPServer
+import SocketServer
+
+#constants
+PORT = 8000
+
+#Recieves request for website.  
+
+#spin up selenium instance
+
+#pulls request data out of web request
+
+#load site
+
+#take screenshot
+
+#serve screenshot
+
+#wait for next request
