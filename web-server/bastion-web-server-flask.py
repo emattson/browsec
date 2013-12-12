d0c1639fa3b11217f524834a018e17279508e4d8
Eli Mattson <emattson@gwu.edu>
2013-12-11 19:18:50 -0500

    add alternate viewing method using divs and extracted content

--- web-server/bastion-web-server-flask.py
+++ web-server/bastion-web-server-flask.py
@@ -2,6 +2,8 @@
 from flask import Flask 
 from flask import render_template
 
+import base64
+
 from seleniumserver import *
 
 #declare app 
@@ -11,12 +13,18 @@ app = Flask(__name__)
 def index():
 	return 'Welcome to Bastion'
 
+@app.route('/hello/<name>')
+def hi(name=None):
+	n = name
+	return render_template("hello.html", data=n)
+
 @app.route('/get_sspng/<url>')
 def get_request(url):
-	app.logger.debug("user requested %s", url)
-	serve = Selenium_Instance()
+	decoded_url = base64.urlsafe_b64decode(str(url))
+	app.logger.debug("user requested screenshot %s", decoded_url)
+	serve = Selenium_Instance(app)
 	#load site
-	serve.get_page("http://" + url)
+	serve.get_page(decoded_url)
 	#take screenshot
 	serve.take_screenshot()
 	#serve screenshot
@@ -24,7 +32,21 @@ def get_request(url):
 	serve.shutdown()
 	return render_template("screenshot-page.html")
 
+@app.route('/get_div/<url>')
+def div_request(url):
+	decoded_url = base64.urlsafe_b64decode(str(url))
+	app.logger.debug("user requested web-elements %s", decoded_url)
+	serve = Selenium_Instance(app)
+	serve.get_page(decoded_url)
+	everything = serve.parse_current_page()
+	app.logger.debug(str(everything))
+	serve.shutdown()
+	return render_template("floating-div.html", title='Bastion', content=everything, url=decoded_url)
 
+@app.after_request
+def add_header(response):
+	response.headers['Cache-Control'] = 'public, max-age=0'
+	return response
 
 if __name__ == '__main__':
 	app.debug = True #True allows for arbitrary code execution!
--- web-server/seleniumserver.py
+++ web-server/seleniumserver.py
@@ -1,12 +1,15 @@
 #selenium server class 
 from selenium import webdriver
 from pyvirtualdisplay import Display
+import base64
 
 class Selenium_Instance:
-	def __init__(self):
+	def __init__(self, app):
 		self.display = Display(visible=0, size=(1024, 768))
 		self.display.start()
 		self.driver = webdriver.Firefox()
+		self.app = app
+		self.app_location = "/get_div/"
 		return
 
 	def get_page(self, url):
@@ -14,6 +17,42 @@ class Selenium_Instance:
 			self.driver = webdriver.Firefox()
 		self.driver.get(url)
 		return
+
+	def get_elements_by_xpath(self, path):
+		if self.driver is None:
+			self.driver = webdriver.Firefox()
+		elements = self.driver.find_elements_by_xpath(path)
+		parsed = []
+		for element in elements:
+			if element.is_displayed():
+				attributes = {'id': element.get_attribute("id")}
+				attributes['name'] = element.get_attribute("name")
+				attributes['class'] = element.get_attribute("class")
+				attributes['text'] = element.text
+				attributes['x'] = element.location['x']
+				attributes['y'] = element.location['y']
+				attributes['height'] = element.size['height']
+				attributes['width'] = element.size['width']
+				attributes['tag'] = element.tag_name
+				attributes['src'] = element.get_attribute("src")
+				attributes['href'] = self.app_location + base64.urlsafe_b64encode(str(element.get_attribute("href")))
+				attributes['action'] = element.get_attribute("action")
+				parsed.append(attributes)
+				self.app.logger.debug("New element added %s", ' '.join([str(item) for item in attributes]))
+		return parsed
+
+	def parse_current_page(self):
+		content = []
+		content = content + self.get_elements_by_xpath("//form")
+		content = content + self.get_elements_by_xpath("//h1")
+		content = content + self.get_elements_by_xpath("//h2")
+		content = content + self.get_elements_by_xpath("//h3")
+		content = content + self.get_elements_by_xpath("//p")
+		content = content + self.get_elements_by_xpath("//a")
+		content = content + self.get_elements_by_xpath("//input")
+		content = content + self.get_elements_by_xpath("//img")
+		content = content + self.get_elements_by_xpath("//button")
+		return content
 
 	def take_screenshot(self):
 		if self.driver is None:
