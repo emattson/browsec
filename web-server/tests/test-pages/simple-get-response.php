acd36ac33ca7d497cc5aacf2aa0c0c60315c6355
Eli Mattson <emattson@gwu.edu>
2014-03-16 21:24:58 -0400

    add form submission test sites for get and post on a simple form

---
+++ web-server/tests/test-pages/simple-get-response.php
@@ -0,0 +1,8 @@
+<html>
+<head>
+	<title>simple get response</title>
+</head>
+<body>
+	<p>Hello <?php echo $_GET['input1']; ?> </p>
+</body>
+</html>
---
+++ web-server/tests/test-pages/simple-post-response.php
@@ -0,0 +1,8 @@
+<html>
+<head>
+	<title>simple post response</title>
+</head>
+<body>
+	<p>Hello <?php echo $_POST['input1']; ?> </p>
+</body>
+</html>
