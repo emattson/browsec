b26cc12f0e001312b7f0255ebcdc4d1b85fb750d
Eli Mattson <emattson@gwu.edu>
2014-03-26 20:14:32 -0400

    python doesn't like dashes only underscores

--- web-server/bastion_tests.py
+++ web-server/bastion_tests.py
@@ -1,12 +1,12 @@
 # import os
-import bastion_web_server_flask.py
+import webserver_bastion
 import unittest
 
 class FlaskrTestCase(unittest.TestCase):
 
     def setUp(self):
-        bastion_web_server_flask.app.config['TESTING'] = True
-        self.app = bastion_web_server_flask.app.test_client()
+        webserver_bastion.app.config['TESTING'] = True
+        self.app = webserver_bastion.app.test_client()
 
     def tearDown(self):
     	pass
